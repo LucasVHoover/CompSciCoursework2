c7bdfdf48c4c2e0bb2b57dcc54109f24d794e456
LucasVHoover <lucasvh@icloud.com>
2023-09-14 07:56:04 +0100

    databasal pain

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ def initialiseTables():
     cursor = connection.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS tree(
-                NodeID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NodeID INTEGER PRIMARY KEY,
                 TreeID INT,
                 name VARCHAR(255),
                 duration INT,
@@ -23,56 +23,58 @@ def initialiseTables():
     ''')
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS accounts(
-                   accountID INTEGER PRIMARY KEY AUTOINCREMENT,
-                   username varchar(20),
+                   accountID INTEGER PRIMARY KEY,
+                   username varchar(20)
     )
     ''')
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS accountsxtrees(
-                   accountID INT
+                   accountID INT,
                    treeID INT
     )
     ''')
 
-def update_Tree_fields(input):
+def update_Tree_fields(input, treeID):
     for node in input:
-        node[2] = ' '.join(node[2])
+        node.insert(0, treeID)
+    for node in input:
         node[3] = ' '.join(node[3])
+        node[4] = ' '.join(node[4])
     for i in range(len(input)):
         input[i] = tuple(input[i])
     return input
 
-def retrieve_treeID(output):
-    return output[0][1]
 
 def retrieve_fields(output):
     for i in range(len(output)):
         output[i] = list(output[i])
     for node in output:
-        node[4] = node[4].split()
         node[5] = node[5].split()
+        node[6] = node[6].split()
+    print(output)
+    TreeID = output[0][0]
     for each in output:
-            del each[:2]
-    return output
+            del each[:3]
+    return output, TreeID
 
-def additemstotree(fields):
+def additemstotree(fields, treeID):
+    committance = update_Tree_fields(fields, treeID)
     connection = sqlite3.connect("activity-tables.db")
     cursor = connection.cursor()
-    for p in fields:
+    print(committance)
+    for p in committance:
         cursor.execute('''
-    INSERT INTO tree
-                    (name, duration, predecessors, successors, EST, LFT, height)
-                    VALUES (?,?,?,?,?,?,?)
+    INSERT INTO tree (TreeID, name, duration, predecessors, successors, EST, LFT, height)
+                       VALUES (?,?,?,?,?,?,?,?)
                     ''', p)
 
 
 def fetchTree():
     connection = sqlite3.connect("activity-tables.db")
     cursor = connection.cursor()
-    output = cursor.execute("SELECT * FROM tree").fetchall()
-    treeID = retrieve_treeID(output)
-    returned_tree = retrieve_fields(output)
-    return returned_tree, treeID
+    output = cursor.execute("SELECT * FROM tree WHERE TreeID = 1").fetchall()
+    print(output)
+    return retrieve_fields(output)
 
 def insertHashword(key, value):
     index = argon2.hash_password(key.encode())
--- logic.py
+++ logic.py
@@ -1,29 +1,30 @@
 #DATA FORMAT ["name (input by user)", "duration  (input by user)", "immediate Predecessors (input by user)",  "immediate successors", EST, LFT, Height]
-tree = [['A',7, [], [], None, None, 0],
-        ['B', 6, [], [], None, None, 0],
-        ['C',  15, [],[], None, None,0],
-        ['D',  9, ['A', 'B'],[], None, None,0],
-        ['E',  8, ['D'],[], None, None,0],
-        ['F', 6, ['C', 'D'],[], None, None, 0],
-        ['G',  7, ['C'],[], None, None,0],
-        ['H',  14, ['E'],[], None, None,0],
-        ['I',  17, ['F', 'G'],[], None, None,0],
-        ['J',  9, ['H','I'],[], None, None,0],
-        ['K',  8, ['I'],[], None, None,0],
-        ['L',  12, ['J', 'K'],[], None, None,0]]
+#tree = [['A',7, [], [], None, None, 0],
+        #['B', 6, [], [], None, None, 0],
+        #['C',  15, [],[], None, None,0],
+        #['D',  9, ['A', 'B'],[], None, None,0],
+        #['E',  8, ['D'],[], None, None,0],
+        #['F', 6, ['C', 'D'],[], None, None, 0],
+        #['G',  7, ['C'],[], None, None,0],
+        #['H',  14, ['E'],[], None, None,0],
+       # ['I',  17, ['F', 'G'],[], None, None,0],
+       # ['J',  9, ['H','I'],[], None, None,0],
+       # ['K',  8, ['I'],[], None, None,0],
+       # ['L',  12, ['J', 'K'],[], None, None,0]]
 
 #add total predecessors value
 
 #go along path of sucession until a you have a point where on predecesor is unresolved. then you turn back
 
-def Immediate_Successors():
+def Immediate_Successors(tree):
     for node in tree:
         for i in range(len(tree)):
             for predecessors in tree[i][2]:
                 if node[0] == predecessors:
                     node[3].append(tree[i][0])
+    return tree
 
-def StartEnd_Nodes():
+def StartEnd_Nodes(tree):
     start_node = ['START', 0, [], [], 0, None, 1]
     end_node = ['END', 0, [], [], None, None, 0]
     for node in tree:
@@ -37,7 +38,9 @@ def StartEnd_Nodes():
     tree.append(start_node)
     tree.append(end_node)
 
-def height(level):
+    return tree
+
+def height(tree, level):
     sucessors = []
     for node in tree:
         if node[6] == level:
@@ -50,15 +53,16 @@ def height(level):
             if node[0] in sucessors:
                 if node[6] <= (level):
                     node[6] = level + 1
-        height(level+1)
+        height(tree, level+1)
+    return tree
 
-def maxheights():
+def maxheights(tree):
     heights = []
     for node in tree:
         heights.append(node[6])
     return max(heights)
   
-def forwardPass(level):
+def forwardPass(tree, level, maxheight):
     for examinednode in tree:
         predecessors = []
         Largest = []
@@ -72,16 +76,19 @@ def forwardPass(level):
         else:
             pass
     if level <= maxheight:
-        forwardPass(level+1)
+        forwardPass(tree, level+1, maxheight)
     else:
         print("finished")
 
-def LFT_EndNode():
+    return tree
+
+def LFT_EndNode(tree, maxheight):
     for node in tree:
         if node[6] == maxheight:
             node[5] = node[4] + node[1]
+    return tree
 
-def backwardPass(level):
+def backwardPass(tree, level):
     for examinednode in tree:
         sucessors = []
         Smallest = []
@@ -97,9 +104,11 @@ def backwardPass(level):
     if level == 0:
         print("finished")
     else:
-        backwardPass(level-1)
+        backwardPass(tree, level-1)
 
-def criticalPathTree():
+    return tree
+
+def criticalPathTree(tree):
     CPtree = tree
     for node in CPtree:
         if (node[1] + node[4]) != node[5]:
@@ -108,7 +117,7 @@ def criticalPathTree():
 
 #NON FUNCTIONAL
 
-def identifyCriticalPaths(position): 
+def identifyCriticalPaths(tree, position): 
     for node in tree:
         if node[0] == position:
             print(node[0])
@@ -117,7 +126,8 @@ def identifyCriticalPaths(position):
                 print("end")
             else:
                 for each in node[3]:
-                    identifyCriticalPaths(each)
+                    identifyCriticalPaths(tree, each)
+    return tree
 
 def resource_levelling():
     pass
